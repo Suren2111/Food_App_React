c0ccf0d9fc08b559fed9646043e50215c1b89280
Suren2111 <surendiran2111@gmail.com>
2024-08-29 13:12:41 +0530

    layout

--- App.js
+++ App.js
@@ -1,16 +1,54 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
+const RestuarantCards=()=>{
+    return(
+            <div className="res-card">
+              <img className="card-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/250d7054-1254-4158-b1a5-67141ea2d51f_25881.JPG" alt="React Image" />
+              <h2>Subway</h2>
+              <h3>Salad, Snacks, Desserts</h3>
+              <h4>Sollinganallur</h4>
+            </div>         
+    )
+}
+
 const Header=() =>{
-    return <h1>Header</h1>
+    return(
+        <div className="header-container">
+            <div className="logo-container">
+                  <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPSrUx8hhIIOCyMTdcrmSb-W3qtYThjB2z7A&s" alt="React Image" />
+            </div>
+            <div className="nav-container">
+                <ul className="nav">
+                   <li>Home</li>
+                   <li>Cart</li>
+                   <li>About Us</li>
+                   <li>Contact Us</li>
+                </ul>
+           </div>
+        </div>
+           
+    )
 }
 
 const Body=() =>{
-    return <h1>Body</h1>
+   return(
+        <div className="res-container">
+            <RestuarantCards />
+            <RestuarantCards />
+            <RestuarantCards />
+            <RestuarantCards />
+            
+        </div>
+    )
 }
 
 const Footer=() =>{
-    return <h1>Footer</h1>
+    return(
+        <div>
+            
+        </div>
+    )
 }
 
 const Applayout=() =>{
