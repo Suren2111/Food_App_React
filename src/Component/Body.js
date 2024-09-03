2958a98be04615026d64265db235a1d0f04c4a7c
Suren2111 <surendiran2111@gmail.com>
2024-09-03 16:06:56 +0530

    Search Functionality

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -4,29 +4,44 @@ import Shimmer from "./Shimmer";
 
 const Body=() =>{
     const [restuarantlist,setRestuarantlist]=useState([]);
+    const [searchText,setSearchText]=useState("");
+    const[filteredreslist,setFilteredreslist]=useState([]);
     useEffect(()=>{
         fetchData();
     },[])
     const fetchData= async () =>{
         const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9005743&lng=80.0931249&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const jsondata=await data.json();
-        console.log(jsondata.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        // console.log(jsondata.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
         setRestuarantlist(jsondata.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredreslist(jsondata.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
     return restuarantlist.length==0 ? <Shimmer/> :(
         <div className="body-container">
+            
            <div className="filter">
+               <div className="search-box">
+                    <input text="text"value={searchText} onChange={(e)=>{
+                       setSearchText(e.target.value);
+                    }}></input>
+                    <button onClick={()=>{
+                        const searchres=restuarantlist.filter((res)=>{
+                           return res.info.name.toLowerCase().includes(searchText.toLowerCase()); 
+                        })
+                        setFilteredreslist(searchres);
+                    }}> Search</button>
+               </div>
                <button className="filter_btn"onClick={()=>{
                    const filter_restuarantlist=restuarantlist.filter(res =>{
                          return res.info.avgRating > 4.5
                         })
-                         setRestuarantlist(filter_restuarantlist);
+                        setFilteredreslist(filter_restuarantlist);
                          }}>Restuarant Filter
                </button> 
            </div>
            <div className="res-container">
              {
-                 restuarantlist.map((restuarant) =>{
+                 filteredreslist.map((restuarant) =>{
                  return <RestuarantCards key={restuarant.info.id} rescard={restuarant}/>
              })
              }
