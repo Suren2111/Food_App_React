9150e8042f7e83b6a1002acdf46b0022e18d66fc
Suren2111 <surendiran2111@gmail.com>
2024-09-25 22:16:43 +0530

    class based components - mounting phase implemented

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,14 +1,33 @@
 import User from "./User"
 import UserClass from "./UserClass";
-const About=()=>{
+import { Component } from "react";
+
+
+class About extends Component{
+
+  constructor(){
+    super();
+    console.log("parent constructor");
+  }
+
+  componentDidMount(){
+    console.log("parent componentDidMount");
+  }
+
+  render(){
+    console.log("parent render")
     return(
       <>
-      <User name={"function"} location={"chennai"} contact={"suren_1999"}/>
-     <UserClass name={"class"} location={"chennai"} contact={"suren_1999"}/>
+
+     <UserClass name={"first"} location={"chennai"} contact={"suren_1999"}/>
+     <UserClass name={"second"} location={"chennai"} contact={"suren_1999"}/>
+     <UserClass name={"third"} location={"chennai"} contact={"suren_1999"}/>
       
       </>
       
     )
+  }
+
 }
 
 export default About;
--- src/Component/UserClass.js
+++ src/Component/UserClass.js
@@ -1,19 +1,31 @@
 import React from "react";
 class UserClass extends React.Component{
-
+    
     constructor(props){
+      
         super(props);
-        console.log(props);
         this.state={
             count:0
         }
+        console.log(this.props.name + "Child constructor")
+       
+        
     }
 
+    componentDidMount(){
+        console.log(this.props.name + "child componentDidMount");
+      }
      render(){
+    console.log(this.props.name + "Child render")
      const{name,location,contact}=this.props;
       return(
         <div>
             <h1>count:{this.state.count}</h1>
+            <button onClick={()=>{
+              this.setState({
+                count:this.state.count+1
+              })
+            }}>Count: {this.state.count}</button>
             <h2>{name}</h2>
             <h3>{location}</h3>
             <h4>{contact}</h4>
